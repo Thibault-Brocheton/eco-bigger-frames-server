34d0c7f77a90b8243ad914bf7cd6cb32b1ece45b
Thibault-Brocheton <brochetonthibault@gmail.com>
2025-09-01 04:05:35 +0200

    add mod registration

---
+++ CavRnMods/BiggerFrames/BiggerFramesMod.cs
@@ -0,0 +1,14 @@
+﻿namespace CavRn.BiggerFrames
+{
+    using Eco.Core.Plugins.Interfaces;
+
+    public class BiggerFramesMod : IModInit
+    {
+        public static ModRegistration Register() => new()
+        {
+            ModName = "BiggerFrames",
+            ModDescription = "Adds bigger frames to all types of frame, and add new borderless frames.",
+            ModDisplayName = "Bigger Frames"
+        };
+    }
+}
